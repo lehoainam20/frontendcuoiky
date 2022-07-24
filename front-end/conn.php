e71f6ef0d8c4e00870c1e69f3a6378daff39630a
QungMinh2802 <20661032@kthcm.edu.vn>
2022-07-24 19:37:38 +0700

    Update conn.php

--- front-end/conn.php
+++ front-end/conn.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "php";
+        protected $severName  = "bmzp8twbzu4zzvmjigvd-mysql.services.clever-cloud.com";
+        protected $userName = "uzezkldlhaupjvye";
+        protected $passWord = "jM0Fkl8yMfDOhnTjF1Tk";
+        protected $databaseName = "bmzp8twbzu4zzvmjigvd";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
